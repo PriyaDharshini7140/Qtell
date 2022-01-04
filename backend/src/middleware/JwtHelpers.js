0232d13c83476b430c0e0eb49a8cb5ae37eb8f2f
priya dharshini <riaharshini1998@gmail.com>
2022-01-04 10:46:58 +0530

    branch change first commit

--- backend/src/middleware/JwtHelpers.js
+++ backend/src/middleware/JwtHelpers.js
@@ -1,4 +1,4 @@
-const {SECRET_KEY,TOKEN_EXPIRE} = require('../configuration/Config');
+const {SECRET_KEY,TOKEN_EXPIRE,REFRESH_SECRET_KEY,REFRESH_TOKEN_EXPIRE} = require('../configuration/Config');
 const jwt = require("jsonwebtoken")
 module.exports={
     CreateToken : (user)=>{
@@ -24,6 +24,22 @@ module.exports={
         res.status(400).json({ error: "Authentication Error: Invalid Token" });
       }
     }
+  },
+  RefreshToken:(user)=>{
+    return jwt.sign({user_id:user._id},REFRESH_SECRET_KEY,{
+        expiresIn:REFRESH_TOKEN_EXPIRE
+    })
+},
+verifyRefreshToken:()=>{
+  return async (refreshToken) => {
+    try {
+      const verified = await jwt.verify(refreshToken, REFRESH_SECRET_KEY);
+      console.log("verification",verified);
+    const user_id = verified.user_id;
+      next();
+    } catch (error) {
+      res.status(400).json({ error: "Authentication Error: Invalid Token" });
+    }
   }
 }
-
+}
