9959d278ab13f2827bc4bfaf781b42ec725e6170
Prpallavi <apprasana36@gmail.com>
2022-01-07 11:27:52 +0530

    prasannabranchcommit

--- backend/src/route/QuestionRoute.js
+++ backend/src/route/QuestionRoute.js
@@ -5,9 +5,9 @@ const router = express.Router()
 
 
 
-router.post('/', JwtHelpers.checkPermission, QuestionController.createQuestion);
+router.post('/',JwtHelpers.checkPermission,  QuestionController.createQuestion);
 router.put('/:id', JwtHelpers.checkPermission, QuestionController.updateQuestion);
-router.get('/view', JwtHelpers.checkPermission, QuestionController.viewQuestion);
-router.get('/particularview/:id', JwtHelpers.checkPermission, QuestionController.viewparticularQuestion);
-router.patch('/approval', JwtHelpers.checkPermission,  QuestionController.approveQuestion);
+router.get('/view',JwtHelpers.checkPermission,  QuestionController.viewQuestion);
+router.get('/particularview/:id',JwtHelpers.checkPermission,  QuestionController.viewparticularQuestion);
+router.patch('/approval', JwtHelpers.checkPermission, QuestionController.approveQuestion);
 module.exports = router
